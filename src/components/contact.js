1d374049ed3b5d98ab23cbdcc9dd8111e9c0b30f
Berk Özdemir <berkozdemir@Berks-MacBook-Pro.local>
2018-06-28 16:12:05 +0300

    h

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,18 +6,18 @@ class Contact extends Component {
         return(
             <div>
                 <h1 className="f1 tc"><span className="red">C͉</span><span className="orange">ȍ</span><span className="yellow">n̋</span><span className="green">t͡</span><span className="blue">a̚</span><span className="purple">c̾</span><span className="pink">tͫ</span><span className="near-black"> M͋e̟</span><span className="white">!̶</span></h1>
-                <div className="pressstart2p flex flex-wrap">
+                <div className="pressstart2p flex flex-wrap items-center">
                     <div className="dib f3 w-50 pa2">
                         <ul>
                             If you need someone to:<br/>
-                            <li className="green">Produce music / beats or remix your tracks</li>
-                            <li className="blue">Do some coding for your art projects</li>
-                            <li className="orange">Make sound design / music for your films, videogames, videos etc.</li>
-                            <li className="purple">Do some coding for your art project</li>
+                            <li className="green mv4">Produce music / beats or remix your tracks</li>
+                            <li className="blue mv4">Do some coding for your art projects</li>
+                            <li className="orange mv4">Make sound design / music for your films, videogames, videos etc.</li>
+                            <li className="purple mv4">Do some coding for your art project</li>
                         </ul>
                     </div>
-                    <div id="contact" className="w-50 pa2">
-                        <p>You can contact me sending an email to berk.ozd@gmail.com</p>
+                    <div id="contact" className="w-50 f2 pa2">
+                        <span className="blink ms-word-art">Send a mail to berk.ozd@gmail.com</span>
                     </div>
                 </div>
             </div>
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import "../css/header.css"
 
 
 
@@ -7,14 +8,14 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
     render() {
         return(
-        <header className="nunito bg-black-60 ph3 pv3 pv3-ns ph4-m ph5-l flex justify-between items-center">
-            <span id='isim' className='white dib f3 fw6 lh-title'>BERK ÖZDEMİR</span>
-            <nav  className="f6 fw6 ttu tracked">
-                <Link className="link dim white dib mr3" to="/" title="Home">Home</Link> 
-                <Link className="link dim white dib mr3" to="/about" title="About">About</Link>
-                <Link className="link dim white dib mr3" to="/portfolio" title="Portfolio">Portfolio</Link>
-                <Link className="link dim white dib mr3" to="/contact" title="Contact">Contact</Link>
-                <Link className="link dim white dib mr3" to="/cv" title="CV">CV</Link>
+        <header className="bg-black-60 ph2 pv2 pv3-ns ph4-m ph5-l flex justify-between items-center">
+            <span id='isim' className='f2 fw6 lh-title'>berkozdemir.com</span>
+            <nav  className="nunito f6 fw6 ttu tracked">
+                <Link id="nah" className="link dim white dib mr3" to="/" title="Home">Home</Link> 
+                <Link id="nah" className="link dim white dib mr3" to="/about" title="About">About</Link>
+                <Link id="nah" className="link dim white dib mr3" to="/portfolio" title="Portfolio">Portfolio</Link>
+                <Link id="nah" className="link dim white dib mr3" to="/contact" title="Contact">Contact</Link>
+                <Link id="nah" className="link dim white dib mr3" to="/cv" title="CV">CV</Link>
             </nav>
         </header>
         )
@@ -23,19 +24,19 @@ class Header extends Component {
 
 const randomColor = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 
-function animateisim(){
-    let title = document.getElementById("isim");
-    let text = title.innerText;
-    let newText = "";
+// function animateisim(){
+//     let title = document.getElementById("isim");
+//     let text = title.innerText;
+//     let newText = "";
     
-    for (let char = 0; char < text.length; char++) {
-        let letter = text[char];
-        newText += letter.fontcolor(randomColor());
-    }
+//     for (let char = 0; char < text.length; char++) {
+//         let letter = text[char];
+//         newText += letter.fontcolor(randomColor());
+//     }
     
-    return title.innerHTML = newText;
-}
+//     return title.innerHTML = newText;
+// }
 
-setInterval(animateisim, 400)
+// setInterval(animateisim, 400)
 
 export default Header;
